b86f9d6388b0d29bac4f2219ed75761812e9870d
WON-DER <wonderfilledyt@gmail.com>
2022-07-02 10:42:14 -0700

    deploy to vercel

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		NEXT_PUBLIC_SPOTIFY_CLIENT_ID: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+		NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET:
+			process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
 	},
 	images: {
 		domains: ['lh3.googleusercontent.com', 'i.scdn.co'],
