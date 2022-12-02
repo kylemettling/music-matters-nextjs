5499e5d491dfdc356ef15324ec1f9ad436baec98
kylemettling <wonderfilledyt@gmail.com>
2022-12-01 16:32:11 -0800

    audioRecorder edits

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,81 +1,82 @@
-import { useState, useEffect } from 'react'
-import { useAudioRecorder } from '../../lib/hooks'
+import { useState, useEffect } from "react";
+import { useAudioRecorder } from "../../lib/hooks";
 
 const TestElement = ({ API_KEY, API_HOST }) => {
-	const [audioURL, setAudioURL] = useState('')
-	let mediaRecorder,
-		chunks = []
-	const { status, stopRecording, startRecording } =
-		useAudioRecorder(mediaRecorder)
-	async function getMedia(contraints) {
-		console.log('ok?', contraints)
-		stream = navigator.mediaDevices
-			.getUserMedia({
-				audio: true,
-			})
-			.then((stream) => {
-				console.log(stream)
-				mediaRecorder = new MediaRecorder(stream)
+  const [audioURL, setAudioURL] = useState("");
+  let mediaRecorder,
+    chunks = [];
+  const { status, stopRecording, startRecording } = useAudioRecorder();
+  async function getMedia(contraints) {
+    console.log("ok?", contraints);
+    stream = navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+      })
+      .then((stream) => {
+        console.log(stream);
+        mediaRecorder = new MediaRecorder(stream);
 
-				mediaRecorder.ondataavailable = (e) => {
-					chunks.push(e.data)
-				}
-				mediaRecorder.onstop = () => {
-					const blob = new Blob(chunks, { type: 'audio/ogg; codex=opus' })
-					chunks = []
-					const audio = window.URL.createObjectURL(blob)
-					setAudioURL(audio)
-					console.log(audio)
-				}
-				// setAudioStream(stream)
-			})
-			.catch((err) => console.log('ERROR!', err))
-		console.log('stream!', stream)
-	}
+        mediaRecorder.ondataavailable = (e) => {
+          chunks.push(e.data);
+        };
+        mediaRecorder.onstop = () => {
+          const blob = new Blob(chunks, { type: "audio/ogg; codex=opus" });
+          chunks = [];
+          const audio = window.URL.createObjectURL(blob);
+          setAudioURL(audio);
+          console.log(audio);
+        };
+        // setAudioStream(stream)
+      })
+      .catch((err) => console.log("ERROR!", err));
+    console.log("stream!", stream);
+  }
 
-	const axios = require('axios')
+  const axios = require("axios");
 
-	const options = {
-		method: 'POST',
-		url: 'https://shazam.p.rapidapi.com/songs/v2/detect',
-		// params: { timezone: 'America/Chicago', locale: 'en-US' },
-		headers: {
-			'content-type': 'text/plain',
-			'X-RapidAPI-Key': API_KEY,
-			'X-RapidAPI-Host': API_HOST,
-		},
-		data: '"Generate one on your own for testing and send the body with the content-type as text/plain"',
-	}
-	console.log(options)
-	// axios
-	// 	.request(options)
-	// 	.then(function (response) {
-	// 		console.log(response.data)
-	// 	})
-	// 	.catch(function (error) {
-	// 		console.error(error)
-	// 	})
+  const options = {
+    method: "POST",
+    url: "https://shazam.p.rapidapi.com/songs/v2/detect",
+    // params: { timezone: 'America/Chicago', locale: 'en-US' },
+    headers: {
+      "content-type": "text/plain",
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": API_HOST,
+    },
+    data: '"Generate one on your own for testing and send the body with the content-type as text/plain"',
+  };
+  console.log(options);
+  // axios
+  // 	.request(options)
+  // 	.then(function (response) {
+  // 		console.log(response.data)
+  // 	})
+  // 	.catch(function (error) {
+  // 		console.error(error)
+  // 	})
 
-	return (
-		<>
-			<audio src={audioURL} controls></audio>
-			<button onClick={(e) => getMedia(e)}>Try me!</button>
-			<button onClick={(e) => clearMedia(e)}>Clear Device</button>
-			<div>
-				<span>Status: {status}</span>
-				<button onClick={(e) => startRecording()}>Start</button>
-				<button onClick={(e) => setStatus()}>Stop</button>
-			</div>
-			<span>{audioURL}</span>
-		</>
-	)
-}
-export default TestElement
+  return (
+    <>
+      <audio src={audioURL} controls></audio>
+      <button onClick={(e) => getMedia(e)}>Try me!</button>
+      <button onClick={(e) => clearMedia(e)}>Clear Device</button>
+      <div>
+        <span>Status: {status}</span>
+        <button onClick={(e) => startRecording({ mediaRecorder })}>
+          Start
+        </button>
+        <button onClick={(e) => setStatus({ mediaRecorder })}>Stop</button>
+      </div>
+      <span>{audioURL}</span>
+    </>
+  );
+};
+export default TestElement;
 
 export async function getStaticProps(context) {
-	const API_KEY = process.env.X_RAPID_API_KEY
-	const API_HOST = process.env.X_RAPID_API_HOST
-	return {
-		props: { API_KEY, API_HOST },
-	}
+  const API_KEY = process.env.X_RAPID_API_KEY;
+  const API_HOST = process.env.X_RAPID_API_HOST;
+  return {
+    props: { API_KEY, API_HOST },
+  };
 }
