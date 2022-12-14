6e857d45e7ea8e1cfa01f863a2b5f43071ed0f98
kylemettling <wonderfilledyt@gmail.com>
2022-12-13 16:53:37 -0800

    auth fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,119 +1,136 @@
-import styles from '../components/header.module.css'
-import '../styles/globals.css'
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { PageWrapper } from '../lib/state'
-import Header from '../components/Header'
-import { supabase } from '../pages/api/supabase'
-import { useRouter } from 'next/router'
-import { ApolloProvider, gql, useQuery } from '@apollo/client'
-import client from '../lib/apollo'
-import { BackButton } from '../components/BackButton'
-import { MdOutlinePalette } from 'react-icons/md'
-import { Shazam } from '../components/Shazam'
+import styles from "../components/header.module.css";
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { PageWrapper } from "../lib/state";
+import Header from "../components/Header";
+// import { supabase } from "../pages/api/supabase";
+import { useRouter } from "next/router";
+import { ApolloProvider, gql, useQuery } from "@apollo/client";
+import client from "../lib/apollo";
+import { BackButton } from "../components/BackButton";
+import { MdOutlinePalette } from "react-icons/md";
+import { Shazam } from "../components/Shazam";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
-function MyApp({ Component, pageProps, API_KEY, API_HOST }) {
-	const [themeToggle, setThemeToggle] = useState(true)
-	const router = useRouter()
-	const [session, setSession] = useState(null)
-	const { pathname } = router
-	function handleThemeToggle(e) {
-		!themeToggle
-			? document.body.classList.remove('dark')
-			: document.body.classList.add('dark')
-		setThemeToggle(!themeToggle)
-	}
+function MyApp({ Component, pageProps, API_KEY, API_HOST, user }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const [themeToggle, setThemeToggle] = useState(true);
+  const router = useRouter();
+  //   const [session, setSession] = useState(null);
+  const { pathname } = router;
+  function handleThemeToggle(e) {
+    !themeToggle
+      ? document.body.classList.remove("dark")
+      : document.body.classList.add("dark");
+    setThemeToggle(!themeToggle);
+  }
 
-	async function signIn() {
-		const env =
-			process.env.NODE_ENV === 'production'
-				? 'https://www.musicmatters.life'
-				: 'http://localhost:3000'
-		const { error, data } = await supabase.auth.signIn(
-			{
-				provider: 'google',
-			},
-			{ redirectTo: env }
-		)
-		console.log(data)
-		if (error) {
-			console.log({ error })
-		}
-	}
-	async function signOut() {
-		const { error } = await supabase.auth.signOut()
-		if (error) {
-			console.log({ error })
-		}
-		router.push('/')
-	}
-	useEffect(() => {
-		setSession(supabase.auth.session())
-		supabase.auth.onAuthStateChange((_event, session) => {
-			if (_event === 'SIGNED_IN') {
-				console.log('signed in :D')
-				setSession(session)
-			}
-			if (_event === 'SIGNED_OUT') {
-				console.log('signed out :O')
-				setSession(null)
-			}
-		})
-	}, [session])
+  async function signIn() {
+    const session = await supabase.auth.getSession();
+    console.log(session);
+    const env =
+      process.env.NODE_ENV === "production"
+        ? "https://www.musicmatters.life"
+        : "http://localhost:3000";
+    const { error, data } = await supabase.auth.signInWithOAuth(
+      {
+        provider: "google",
+      },
+      { redirectTo: env }
+    );
+    console.log(data);
+    if (error) {
+      console.log({ error });
+    }
+  }
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log({ error });
+    }
+    router.push("/");
+  }
+  // useEffect(() => {
+  //   setSession(supabase.auth.session())
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     if (_event === "SIGNED_IN") {
+  //       console.log("signed in :D");
+  //       setSession(session);
+  //     }
+  //     if (_event === "SIGNED_OUT") {
+  //       console.log("signed out :O");
+  //       setSession(null);
+  //     }
+  //   });
+  // }, [session]);
 
-	return (
-		<PageWrapper>
-			<ApolloProvider client={client}>
-				<nav>
-					{' '}
-					{pathname !== '/' && <BackButton />}
-					<Link href='/'> Home</Link>
-					{session ? (
-						<>
-							<Link href='/profile'>
-								{' '}
-								{session?.user?.user_metadata?.full_name}
-							</Link>
-							<Link href='#' onClick={() => signOut()}>
-								Sign Out
-							</Link>
-						</>
-					) : (
-						<Link href='#' onClick={() => signIn()}>
-							Sign In
-						</Link>
-					)}{' '}
-					<a>
-						<MdOutlinePalette
-							tabIndex='0'
-							aria-label='theme select'
-							className={`${styles.themeSelect} ${
-								!themeToggle
-									? styles.themeSelectBackward
-									: styles.themeSelectForward
-							}`}
-							onKeyDown={(e) =>
-								e.key === 'Enter' ? handleThemeToggle(e) : null
-							}
-							onKeyUp={() => null}
-							onClick={(e) => handleThemeToggle(e)}
-						/>
-					</a>
-				</nav>
-				<Header
-					includeBackButton={router.pathname !== '/' ? true : false}
-					session={session}
-				/>
-				<Component {...pageProps} session={session} />
-				<style jsx>{``}</style>
-			</ApolloProvider>
-		</PageWrapper>
-	)
+  return (
+    <PageWrapper>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <ApolloProvider client={client}>
+          <nav>
+            {" "}
+            {pathname !== "/" && <BackButton />}
+            <Link href="/"> Home</Link>
+            {pageProps.initialSession ? (
+              <>
+                <Link href="/profile">
+                  {" "}
+                  {
+                    pageProps.initialSession.session?.user?.user_metadata
+                      ?.full_name
+                  }
+                  {/* {session?.user?.user_metadata?.full_name} */}
+                </Link>
+                <Link href="#" onClick={() => signOut()}>
+                  Sign Out
+                </Link>
+              </>
+            ) : (
+              <Link href="#" onClick={() => signIn()}>
+                Sign In
+              </Link>
+            )}{" "}
+            <a>
+              <MdOutlinePalette
+                tabIndex="0"
+                aria-label="theme select"
+                className={`${styles.themeSelect} ${
+                  !themeToggle
+                    ? styles.themeSelectBackward
+                    : styles.themeSelectForward
+                }`}
+                onKeyDown={(e) =>
+                  e.key === "Enter" ? handleThemeToggle(e) : null
+                }
+                onKeyUp={() => null}
+                onClick={(e) => handleThemeToggle(e)}
+              />
+            </a>
+          </nav>
+          <Header
+            includeBackButton={router.pathname !== "/" ? true : false}
+            // session={session}
+          />
+          <Component
+            {...pageProps}
+            //   session={session}
+          />
+          <style jsx>{``}</style>
+        </ApolloProvider>
+      </SessionContextProvider>
+    </PageWrapper>
+  );
 }
 
-export default MyApp
+export default MyApp;
 {
-	/* <Image
+  /* <Image
 											src={session?.user?.user_metadata.picture}
 											height={60}
 											width={60}
