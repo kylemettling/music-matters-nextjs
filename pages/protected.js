2485da59bcf7351f5edc851fbbbc179e8a701c5f
WON-DER <wonderfilledyt@gmail.com>
2022-12-14 11:13:05 -0800

    supabase auth fix

--- pages/protected.js
+++ pages/protected.js
@@ -1,28 +1,24 @@
-import { supabase } from "../pages/api/supabase";
-import Image from "next/image";
+import Image from 'next/image'
+import { useUser, useSession } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+export default function Protected() {
+	const router = useRouter()
+	const session = useSession()
+	const user = useUser()
+	if (!session) router.push('/sign-in')
 
-export default function Protected({ user }) {
-  return (
-    <div>
-      <h2>Hello from protected route!</h2>
-      <p>{JSON.stringify(user)}</p>
-      <p>{JSON.stringify(user.user_metadata.avatar_url)}</p>
-      <Image
-        src={user.user_metadata.avatar_url}
-        height="400px"
-        width="400px"
-        alt={`${user.user_metadata.full_name}`}
-      />
-    </div>
-  );
-}
-
-export async function getServerSideProps({ req }) {
-  const { user } = await supabase.auth.api.getUserByCookie(req);
-
-  if (!user) {
-    return { props: {}, redirect: { destination: "/sign-in" } };
-  }
-
-  return { props: { user } };
+	return (
+		<div>
+			<h2>Hello from protected route!</h2>
+			<p>{JSON.stringify(user)}</p>
+			<p>{JSON.stringify(user.user_metadata.avatar_url)}</p>
+			<Image
+				src={user.user_metadata.avatar_url}
+				height='400px'
+				width='400px'
+				alt={`${user.user_metadata.full_name}`}
+			/>
+		</div>
+	)
 }
