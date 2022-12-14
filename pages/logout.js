b6ce6dbd7ed56fa988965b2ece594d7ad30f407b
WON-DER <wonderfilledyt@gmail.com>
2022-12-14 11:15:27 -0800

    supabase auth fix

--- pages/logout.js
+++ pages/logout.js
@@ -1,10 +1,16 @@
-import { supabase } from '../pages/api/supabase'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import {
+	useUser,
+	useSession,
+	useSupabaseClient,
+} from '@supabase/auth-helpers-react'
 
 export default function Logout() {
 	const router = useRouter()
-
+	const user = useUser()
+	const session = useSession()
+	const supabase = useSupabaseClient()
 	useEffect(() => {
 		const initialize = async () => await supabase.auth.api.signOut()
 
